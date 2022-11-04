ea7b38b5da6cd6913707499b63798a8620000e65
AnastasiaNech <nechaevaanastasiamih@gmail.com>
2022-11-04 19:35:17 +0300

    Add grpc

--- PlatformService/Profiles/PlatformsProfile.cs
+++ PlatformService/Profiles/PlatformsProfile.cs
@@ -8,9 +8,15 @@ namespace PlatformService.Profiles
     {
         public PlatformsProfile()
         {
+            // Source -> Target
             CreateMap<Platform, PlatformReadDto>();
-            CreateMap<PlatformCreateDto,Platform>();
+            CreateMap<PlatformCreateDto, Platform>();
             CreateMap<PlatformReadDto, PlatformPublishedDto>();
+            //Смотри файл platforms.proto
+            CreateMap<Platform, GrpcPlatformModel>()
+                .ForMember(dest => dest.PlatformId, opt => opt.MapFrom(src =>src.Id))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src =>src.Name))
+                .ForMember(dest => dest.Publisher, opt => opt.MapFrom(src =>src.Publisher));
         }
     }
 }
--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -14,6 +16,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using PlatformService.AsyncDataServices;
 using PlatformService.Data;
+using PlatformService.SyncDataServices.Grpc;
 using PlatformService.SyncDataServices.Http;
 using Sentry;
 
@@ -39,10 +42,9 @@ namespace PlatformService
             services.AddScoped<IPlatformRepo, PlatformRepo>();
             services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
             services.AddSingleton<IMessageBusClient, MessageBusClient>();
-
-            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
+            services.AddGrpc();
             services.AddControllers();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PlatformService", Version = "v1" });
@@ -60,7 +62,7 @@ namespace PlatformService
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PlatformService v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
@@ -68,6 +70,12 @@ namespace PlatformService
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapGrpcService<GrpcPlatformService>();
+
+                endpoints.MapGet("/protos/platforms.proto", async context =>
+                {
+                    await context.Response.WriteAsync(File.ReadAllText("Protos/platforms.proto"));
+                });
             });
 
             PredDb.PredPopulation(app, true);
